712fb51609b39ed22fc2e0ee62d16b484b7ab2bb
Joel Denning <joeldenning@gmail.com>
2020-04-13 18:58:08 -0600

    Removing console logs

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,7 +3,6 @@ import { version } from "./fixtures/foo.js";
 
 describe(`basic usage`, () => {
   it(`overrides the default nodejs resolution`, () => {
-    console.log("version", version);
     assert.equal(version, "overridden");
   });
 });
