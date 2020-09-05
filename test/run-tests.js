b1e13625397b6edf56b1b15cf81f6cb37ed9c83e
Joel Denning <joeldenning@gmail.com>
2020-09-04 18:45:56 -0600

    Fixes, including packages via trailing slashes.

--- test/run-tests.js
+++ test/run-tests.js
@@ -4,6 +4,7 @@ const mocha = new Mocha();
 
 mocha.addFile("./test/basic.test.js");
 mocha.addFile("./test/scopes.test.js");
+mocha.addFile("./test/trailing-slashes.test.js");
 
 mocha.loadFilesAsync().then(() => {
   mocha.run();
